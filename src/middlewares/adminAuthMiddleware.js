7b705af6321fb20e0e44d888c7378f85ade1e980
jonatandiazadso <jonatandiazdorado@gmail.com>
2024-03-27 01:00:45 -0500

    funcional token

--- src/middlewares/adminAuthMiddleware.js
+++ src/middlewares/adminAuthMiddleware.js
@@ -2,10 +2,9 @@ import jwt from 'jsonwebtoken';
 import { ADMIN_TOKEN_SECRET } from '../config.js';
 
 export const adminAuthRequired = (req, res, next) => {
-    // Imprimir los encabezados de la solicitud
-    console.log('Headers:', req.headers);
+    // Obtener el token del encabezado de autorización
+    const token = req.headers.authorization;
 
-    const token = req.headers.authorization || req.cookies;
     if (!token) {
         console.log('No token found');
         return res.status(400).json({ message: 'No token, authorization denied' });
@@ -14,7 +13,8 @@ export const adminAuthRequired = (req, res, next) => {
     // Imprimir el token recibido
     console.log('Token:', token);
 
-    jwt.verify(token, ADMIN_TOKEN_SECRET, (err, user) => {
+    // Verificar y decodificar el token
+    jwt.verify(token.split(' ')[1], ADMIN_TOKEN_SECRET, (err, user) => {
         if (err) {
             console.log('Invalid token:', err);
             return res.status(403).json({ message: 'Invalid token' });
